bcd7fcfe1adf8d0551a8e1b0567f30b8c13e2fe7
lellywang <larrywang2019@u.northwestern.edu>
2017-06-27 17:04:06 -0500

    model (no changes need to be made)

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ class MyLSTM(nn.Module):
                     Variable(torch.zeros(self.layers * self.direction, length, self.hidden_dim)))
 
     def forward(self, input_tensor, hidden):
-        # output of (sequence_length, batch_size, hidden_size*directions)
+        ## output of (sequence_length, batch_size, hidden_size*directions)
         output_tensor, hidden = self.lstm(input_tensor, hidden)
         output_tensor = output_tensor.contiguous().view(output_tensor.size(0)*output_tensor.size(1), output_tensor.size(2))
         if self.direction == 2:
