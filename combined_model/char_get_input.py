7f59b6b9300e0fceba83da638cc5bdc23a3fc318
Sangrin Lee <sangrinlee2018@u.northwestern.edu>
2017-07-29 22:33:47 -0500

    convert all xmls into combined text

---
+++ combined_model/char_get_input.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import re
+import glob
+import string
+
+with open("char_data/old_books.txt", "w", encoding='UTF-8', newline='') as write_file:
+	for filename in glob.glob('word_output/*.xml'):
+		with open(filename, "r", encoding='UTF-8', newline='') as read_file:
+			first = True
+			for line in read_file:
+				if re.match(".*lemma=.*", line):
+					word = line.split("</w>")[0].split(">")[1]
+					if first == True:
+						write_file.write(word)
+						first = False
+					else:
+						write_file.write(" " + word)
+				elif re.match(".*</pc>", line):
+					word = line.split("</pc>")[0].split(">")[1]
+					write_file.write(word)
