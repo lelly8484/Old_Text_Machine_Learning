9fd83ab609af7b65ee6df3feafafbe6528dec66a
Sangrin Lee <sangrinlee2018@u.northwestern.edu>
2017-08-03 23:39:00 -0500

    Optimized searching part

--- combined_model/freq_train.py
+++ combined_model/freq_train.py
@@ -4,6 +4,8 @@ from random import randint
 import re
 import operator
 import pickle
+import time
+import string
 
 def star_unique_file(input_filename, output_filename):
 	input_file = open(input_filename, 'r', encoding='UTF-8', newline='')
@@ -37,13 +39,52 @@ index=0
 correct=0
 confidentcounter=0
 dots=[]
+
+
+alphabet = []
+wlist = []
+for letter in string.ascii_letters:
+	alphabet.append(letter)
+	wlist.append([])
+
+for w in unique_words:
+	for alpha in alphabet:
+		if alpha in w:
+			wlist[alphabet.index(alpha)].append(w)
+start_time = time.time()
+# print (wlist[1])
+# exit()
 for i in star_words:
 	print("##" * 33)
 	checker=0
 	predictions={}
 	check_one=0
-	for j in unique_words:
+
+	sel = ''
+	min = -1
+	for rand in i:
+		if not rand == '●':
+			sel = rand # added
+			break # added
+
+			# add below 
+			# if min == -1:
+			# 	min = len(wlist[alphabet.index(rand)])
+			# elif min > len(wlist[alphabet.index(rand)]):
+			# 	min = len(wlist[alphabet.index(rand)])
+			# 	sel = rand
+
+	if sel == '':
+		sel_words = unique_words
+	else:
+		sel_words = wlist[alphabet.index(sel)]
+	# print (unique_words)		
+	# exit()
+
+	for j in sel_words:
 		if len(i)==len(j):
+			# print (len(i), i, len(j), j)
+
 			for x in range(len(i)):
 				if i[x]==j[x]:
 					check_one=1
@@ -70,6 +111,7 @@ for i in star_words:
 		temp4.append(maximum.split(" ")[1])
 		print("Dotted and Prediction: "+maximum)
 		if predictions[maximum]/sum(predictions.values())>.9:
+			print (predictions[maximum])
 			confidentcounter+=1
 		print("Max Frequency:", predictions[maximum]," Percentage frequency over matches:",str(round(100*predictions[maximum]/sum(predictions.values()),2))+"%")
 		correct+=1
@@ -79,6 +121,9 @@ for i in star_words:
 print("##" * 33)
 print("Percentages Matches Found:"+str(100*correct/len(star_words))+"%")
 print("Confident Frequencies (Over .75): "+str(100*confidentcounter/correct)+"%")
+print (confidentcounter, correct)
+end_time = time.time()
+print ("Time Elapsed : ", end_time - start_time)
 
 dot_file=open('freq_data/predicted', 'wb')
 pickle.dump(temp4, dot_file)
